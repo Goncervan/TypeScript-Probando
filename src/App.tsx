8dc24ae3b03e6f57149b761fe8d98d63368f6f74
Goncervan <gonfedecer@gmail.com>
2022-02-21 17:45:03 -0300

    Terminada

--- src/App.tsx
+++ src/App.tsx
@@ -39,43 +39,46 @@ function App(): JSX.Element {
   }
 
   return (
-    <div className="container p-4">
-      <div className="row">
-        <div className="col-md6">
-          <div className="card">
-            <div className="card-body">
-              <form onSubmit={handleSubmit}>
-                <input
-                  ref={taskInput}
-                  className="form-control"
-                  type="text"
-                  placeholder="Nueva tarea"
-                  value={newTask}
-                  onChange={e => setNewTask(e.target.value)}
-                  autoFocus
-                />
-                <button className="btn btn-outline-dark mt-2">
-                  Guardar
-                </button>
-              </form>
+    <div className="principal">
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md6">
+            <div className="card">
+              <div className="card-body div-form">
+                <h1 className="text-center">Mi primera app con TypeScript</h1>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    ref={taskInput}
+                    className="form-control"
+                    type="text"
+                    placeholder="Nueva tarea"
+                    value={newTask}
+                    onChange={e => setNewTask(e.target.value)}
+                    autoFocus
+                  />
+                  <button className="btn w-100 btn-outline-dark mt-2">
+                    Guardar
+                  </button>
+                </form>
+              </div>
             </div>
+            {
+              tasks.map((t: ITask, i: number) => {
+                return (
+                  <div key={i} className="card text-center card-body mt-2">
+                    <h1 className="title" style={{ textDecoration: t.done ? "line-through" : "" }}>{t.name}</h1>
+                    <div className="btns text-center">
+                      <button className="btn btn-secondary" onClick={() => toggleDoneTask(i)}>
+                        {t.done ? "✓" : "✗"}
+                      </button>
+                      <button className="btn btn-danger" onClick={() => removeTask(i)}>
+                        🗑
+                      </button>
+                    </div>
+                  </div>)
+              })
+            }
           </div>
-          {
-            tasks.map((t: ITask, i: number) => {
-              return (
-                <div key={i} className="card card-body mt-2">
-                  <h1 style={{ textDecoration: t.done ? "line-through" : "" }}>{t.name}</h1>
-                  <div>
-                    <button className="btn btn-secondary" onClick={() => toggleDoneTask(i)}>
-                      {t.done ? "✓" : "✗"}
-                    </button>
-                    <button className="btn btn-danger" onClick={() => removeTask(i)}>
-                      🗑
-                    </button>
-                  </div>
-                </div>)
-            })
-          }
         </div>
       </div>
     </div>
